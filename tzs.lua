3b45a9d416596e5a5d78659786050474f6f57115
Tim Smith <tzs@tzs.net>
2009-10-21 08:21:50 +0000

    count tier populations

--- tzs.lua
+++ tzs.lua
@@ -60,6 +60,11 @@ function tzs.OnSlash(args)
         tzs.AddItem( L"", -1, level)
     end
     tzs.count = 0
+    tzs.countT1 = 0
+    tzs.countT2 = 0
+    tzs.countT3 = 0
+    tzs.countT4 = 0
+    tzs.count40 = 0
     tzs.overflow = 0
     tzs.classes = {}
     RegisterEventHandler( SystemData.Events.SOCIAL_SEARCH_UPDATED, "tzs.OnSearch" )
@@ -72,11 +77,28 @@ function tzs.OnSearch()
     then
         if ( #data < 30 ) then
             tzs.count = tzs.count + #data
+            local level = tzs.queue[tzs.readpos+2]
+            if ( level <= 11 ) then
+                tzs.countT1 = tzs.countT1 + #data
+            elseif ( level <= 21 ) then
+                tzs.countT2 = tzs.countT2 + #data
+            elseif ( level <= 31 ) then
+                tzs.countT3 = tzs.countT3 + #data
+            else
+                tzs.countT4 = tzs.countT4 + #data
+                if ( level == 40 ) then
+                    tzs.count40 = tzs.count40 + #data
+                end
+            end
         else
             if ( tzs.queue[tzs.readpos] == L"" )
             then
-                for i = 1, 24 do
-                    tzs.AddItem( tzs.careers[i], tzs.queue[tzs.readpos+1], tzs.queue[tzs.readpos+2] )
+                local offset = 0
+                if ( GameData.Player.realm == GameData.Realm.DESTRUCTION ) then
+                    offset = 12
+                end
+                for i = 1, 12 do
+                    tzs.AddItem( tzs.careers[i+offset], tzs.queue[tzs.readpos+1], tzs.queue[tzs.readpos+2] )
                 end
             else
                 tzs.overflow = tzs.overflow + 1
@@ -87,6 +109,11 @@ function tzs.OnSearch()
     if ( tzs.writepos > tzs.readpos ) then
         tzs.Search()
     else
+        EA_ChatWindow.Print(towstring("tzs: T1 players found: "..tzs.countT1))
+        EA_ChatWindow.Print(towstring("tzs: T2 players found: "..tzs.countT2))
+        EA_ChatWindow.Print(towstring("tzs: T3 players found: "..tzs.countT3))
+        EA_ChatWindow.Print(towstring("tzs: T4 players found: "..tzs.countT4))
+        EA_ChatWindow.Print(towstring("tzs: Level 40 players found: "..tzs.count40))
         EA_ChatWindow.Print(towstring("tzs: total players found: "..tzs.count))
         EA_ChatWindow.Print(towstring("tzs: hit max: "..tzs.overflow))
     end
